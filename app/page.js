fda1c77b5ada01dffc4beb79d6f304ec1c8694e0
Praneetkundaram <praneetkundaram07@gmail.com>
2025-06-05 16:41:50 +0530

    bug fixing

--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
+import { Suspense } from "react";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-full">
-      <Hero />
-      <Footer />
-    </div>
+    <Suspense
+      fallback={
+        <div className="w-full h-full flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
+      <div className="relative w-full h-full">
+        <Hero />
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
